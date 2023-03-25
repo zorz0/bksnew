922f2a64bdb5b34f569465dacf12f06bd6507cf9
Khalid-Abed <73119802+Khalid-Abed@users.noreply.github.com>
2023-03-25 03:51:09 +0200

    prepare layout of project | Khaled

---
+++ resources/views/front-layout/footer.blade.php
@@ -0,0 +1,58 @@
+  <footer class="u-clearfix u-custom-color-3 u-footer u-footer" id="sec-350b">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <div class="u-clearfix u-expanded-width u-gutter-30 u-layout-wrap u-layout-wrap-1">
+        <div class="u-gutter-0 u-layout">
+          <div class="u-layout-row">
+            <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-20 u-layout-cell-1">
+              <div class="u-container-layout u-container-layout-1">
+                <p
+                  class="u-align-right u-custom-font u-small-text u-text u-text-default u-text-variant u-text-white u-text-1">
+                  <a href="/index.html"
+                    class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-active-custom-color-2 u-text-hover-custom-color-2 u-text-white u-btn-1">
+                    <span style="font-size: 1.125rem;"></span>
+                  </a>
+                  <span style="font-size: 1.125rem;">تواصل معنا <br>
+                  </span>07XXXXXXX <br>info@beitkhayratsouf.com
+                </p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-layout-cell u-size-20 u-layout-cell-2">
+              <div class="u-container-layout u-valign-middle u-container-layout-2">
+                <p
+                  class="u-align-right u-custom-font u-small-text u-text u-text-default u-text-variant u-text-white u-text-2">
+                  <a href="/الرئيسية.html"
+                    class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-active-custom-color-2 u-text-hover-custom-color-2 u-text-white u-btn-2">
+                    <span style="font-size: 1rem;"></span>
+                  </a>
+                  <span style="font-size: 1rem;">مواقع التواصل الإجتماعي <br>
+                  </span>الفيسبوك <br>إنستجرام <br>تيك توك <br>اليوتيوب
+                </p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-20 u-layout-cell-3">
+              <div class="u-container-layout u-container-layout-3">
+                <p
+                  class="u-align-right u-custom-font u-small-text u-text u-text-default u-text-variant u-text-white u-text-3">
+                  <a href="/index.html"
+                    class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-active-custom-color-2 u-text-hover-custom-color-2 u-text-white u-btn-3">الرئيسية<br>
+                  </a>
+                  أخبارنا
+                  <br>
+                  المتجر
+                  <br>
+                  من نحن
+                  <br>
+                  تواصل معنا
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="u-align-center u-custom-font u-text u-text-body-alt-color u-text-4"> Powered By Banana </p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
---
+++ resources/views/front-layout/header.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="ar-SA">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="keywords" content="بيت خيرات سوف">
+  <meta name="description" content="بناء من عام 1881م كان منزل لعائلة مسيحية | عائلة البطارسة | وإحدى أركانه كنيسة يقيموا فيها الصلوات.
+حجارة البيت تم نقلها من دولة فلسطين على الجمال انتقل أصحابه من المنزل وتم هجره لمدة 24 سنة.">
+  <title>بيت خيرات سوف | الرئيسية</title>
+  <link rel="stylesheet" href="{{ asset('css/nicepage.css')}}" media="screen">
+  <link rel="stylesheet" href="{{ asset('css/news.css')}}" media="screen">
+  <link rel="stylesheet" href="{{ asset('css/style.css')}}" media="screen">
+  <script class="u-script" type="text/javascript" src="{{ asset('js/jquery-1.9.1.min.js')}}" defer=""></script>
+  <script class="u-script" type="text/javascript" src="{{ asset('js/nicepage.js')}}" defer=""></script>
+  <meta name="generator" content="Nicepage 5.5.0, nicepage.com">
+  <meta property="og:title" content="بيت خيرات سوف">
+  <meta property="og:description" content="بناء من عام 1881م كان منزل لعائلة مسيحية | عائلة البطارسة | وإحدى أركانه كنيسة يقيموا فيها الصلوات.
+حجارة البيت تم نقلها من دولة فلسطين على الجمال انتقل أصحابه من المنزل وتم هجره لمدة 24 سنة.">
+  <meta property="og:image" content="{{ asset('images/logo.png')}}">
+  <meta property="og:url" content="#">
+
+  <link id="u-theme-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+  <link id="u-page-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "name": "BKSDesign",
+      "url": "/",
+      "logo": "{{ asset('images/logo.png')}}"
+    }
+  </script>
+  <meta name="theme-color" content="#478ac9">
+  <meta property="og:type" content="website">
+  <link rel="canonical" href="/">
+</head>
+
+<body class="u-body u-stick-footer u-xl-mode">
+  <header class="u-clearfix u-header u-image lazyload u-header">
+    <a href="#" class="u-align-center u-image u-logo u-image-1">
+      <img src="{{ asset('images/logo.png')}}" class="u-logo-image u-logo-image-1">
+    </a>
+    <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+      <div class="menu-collapse u-custom-font">
+        <a class="u-button-style u-custom-active-border-color u-custom-active-color u-custom-border u-custom-border-color u-custom-border-radius u-custom-borders u-custom-color u-custom-custom-border-radius u-custom-hover-border-color u-custom-hover-color u-custom-left-right-menu-spacing u-custom-text-active-color u-custom-text-color u-custom-text-decoration u-custom-text-hover-color u-custom-text-shadow u-custom-top-bottom-menu-spacing u-nav-link"
+          href="#">
+
+          <svg class="u-svg-link" viewBox="0 0 24 24">
+            <use xlink:href="#menu-hamburger"></use>
+          </svg>
+          <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px"
+            xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <rect y="1" width="16" height="2"></rect>
+              <rect y="7" width="16" height="2"></rect>
+              <rect y="13" width="16" height="2"></rect>
+            </g>
+          </svg>
+        </a>
+      </div>
+      <div class="u-custom-menu u-nav-container">
+        <ul class="u-custom-font u-nav u-spacing-2 u-unstyled u-nav-1">
+          <li class="u-nav-item">
+            <a class="u-active-custom-color-2 u-bottom-left-radius-50 u-bottom-right-radius-50 u-button-style u-hover-custom-color-2 u-nav-link u-text-active-white u-text-hover-white"
+              href="#">
+              تواصل معنا
+            </a>
+          </li>
+          <li class="u-nav-item">
+            <a class="u-active-custom-color-2 u-bottom-left-radius-50 u-bottom-right-radius-50 u-button-style u-hover-custom-color-2 u-nav-link u-text-active-white u-text-hover-white"
+              href="#">
+              من نحن
+            </a>
+          </li>
+          <li class="u-nav-item active">
+            <a class="u-active-custom-color-2 u-bottom-left-radius-50 u-bottom-right-radius-50 u-button-style u-hover-custom-color-2 u-nav-link u-text-active-white u-text-hover-white"
+              href="/index.html">
+              الرئيسية
+            </a>
+          </li>
+          <li class="u-nav-item">
+            <a class="u-active-custom-color-2 u-bottom-left-radius-50 u-bottom-right-radius-50 u-button-style u-hover-custom-color-2 u-nav-link u-text-active-white u-text-hover-white"
+              href="#">
+              المتجر
+            </a>
+          </li>
+          <li class="u-nav-item">
+            <a class="u-active-custom-color-2 u-bottom-left-radius-50 u-bottom-right-radius-50 u-button-style u-hover-custom-color-2 u-nav-link u-text-active-white u-text-hover-white"
+              href="#">
+              أخبارنا
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="u-custom-menu u-nav-container-collapse">
+        <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+          <div class="u-inner-container-layout u-sidenav-overflow">
+            <div class="u-menu-close"></div>
+            <ul
+              class="u-align-center u-custom-font u-nav u-popupmenu-items u-text-active-custom-color-2 u-text-hover-custom-color-2 u-unstyled u-nav-2">
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link" href="/index.html">الرئيسية</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link" href="#">
+                  من نحن</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link" href="#">
+                  المتجر</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link" href="#">
+                  أخبارنا</a>
+              </li>
+              <li class="u-nav-item">
+                <a class="u-button-style u-nav-link" href="#">
+                  تواصل معنا</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+      </div>
+    </nav>
+  </header>
